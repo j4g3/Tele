912103f4b6264680a4d950ac318e630264d58ce5
João <j4g3dev@gmail.com>
2021-07-19 20:09:11 -0300

    fix: :zap: Resolve a bug of no send form

--- front/src/pages/admin/usuarios/usuarios.cadastrar.js
+++ front/src/pages/admin/usuarios/usuarios.cadastrar.js
@@ -1,66 +1,68 @@
-import React, {useState,useEffect} from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
+import React, { useState, useEffect } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
 
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
 
-import api from '../../../services/api';
+import api from "../../../services/api";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(3),
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-
-
 }));
 
 export default function Usuario() {
   const classes = useStyles();
 
-  const [nome , setNome] = useState('');
-  const [sobrenome , setSobrenome] = useState('');
-  const [email , setEmail] = useState('');
-  const [senha , setSenha] = useState('');
-  const [tipo , setTipo] = useState('');
+  const [nome, setNome] = useState("");
+  const [sobrenome, setSobrenome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [tipo, setTipo] = useState("");
 
-async function handleSubmit(){
- 
-
-    const response = await api.post('/api/usuarios');
-    
-    if(response.status===200){
-      window.location.href='/admin/usuarios'
-    }else
-      alert ('Erro ao cadastrar-se')
-}
-
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await api.post("/api/usuarios", {
+        nome_usuario: nome,
+        sobrenome_usuario: nome,
+        email_usuario: email,
+        senha_usuario: senha,
+        tipo_usuario: tipo,
+      });
+      window.location.href = "/usuarios";
+    } catch (err) {
+      alert("Erro ao cadastrar-se");
+    }
+  }
 
   return (
     <Container component="main" maxWidth="xs">
@@ -72,7 +74,7 @@ async function handleSubmit(){
         <Typography component="h1" variant="h5">
           Cadastro
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} onSubmit={handleSubmit} noValidate>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
@@ -84,8 +86,7 @@ async function handleSubmit(){
                 label="Nome"
                 autocompleto="nome"
                 value={nome}
-                onChange={e => setNome(e.target.value)}
-              
+                onChange={(e) => setNome(e.target.value)}
               />
             </Grid>
             <Grid item xs={12} sm={6}>
@@ -95,83 +96,83 @@ async function handleSubmit(){
                 fullWidth
                 id="sobrenome"
                 name="sobrenome"
-                label="Sobrenome"               
+                label="Sobrenome"
                 autocompleto="sobrenome"
                 value={sobrenome}
-                onChange={e => setSobrenome(e.target.value)}
+                onChange={(e) => setSobrenome(e.target.value)}
               />
             </Grid>
             <Grid item xs={12}>
               <TextField
-              variant="outlined"
-              required
-              fullWidth
-              id="Email"
-              name="email"
-              label="Email"
-              autocompleto="email"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
+                variant="outlined"
+                required
+                fullWidth
+                id="Email"
+                name="email"
+                label="Email"
+                autocompleto="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </Grid>
             <Grid item xs={12}>
               <TextField
-              type='password'
-              variant="outlined"
-              required
-              fullWidth
-              id="senha"
-              name="senha"
-              label="Senha"
-              autocompleto="senha"
-              value={senha}
-              onChange={e => setSenha(e.target.value)}
+                type="password"
+                variant="outlined"
+                required
+                fullWidth
+                id="senha"
+                name="senha"
+                label="Senha"
+                autocompleto="senha"
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
               />
             </Grid>
 
             <Grid item xs={12}>
-            <FormControl helperText="Full width!"
-          fullWidth variant="outlined" required className={classes.formControl} >
-        <InputLabel id="labeltipo">Tipo</InputLabel>
-        <Select
-          labelId="labeltipo"
-          id="tipo"
-          value={tipo}
-          onChange={e => setTipo(e.target.value)}
-        >
-          <MenuItem value={10}>Médico</MenuItem>
-          <MenuItem value={20}>Paciente</MenuItem>
-        </Select>
-      </FormControl>
+              <FormControl
+                helperText="Full width!"
+                fullWidth
+                variant="outlined"
+                required
+                className={classes.formControl}
+              >
+                <InputLabel id="labeltipo">Tipo</InputLabel>
+                <Select
+                  labelId="labeltipo"
+                  id="tipo"
+                  value={tipo}
+                  onChange={(e) => setTipo(e.target.value)}
+                >
+                  <MenuItem value={10}>Médico</MenuItem>
+                  <MenuItem value={20}>Paciente</MenuItem>
+                </Select>
+              </FormControl>
             </Grid>
-          
-         
           </Grid>
           <Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            onClick={()=> handleSubmit} 
-            className={classes.submit}
-            
-          >
-            Enviar
-          </Button>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              onClick={() => handleSubmit}
+              className={classes.submit}
+            >
+              Enviar
+            </Button>
           </Grid>
           <Grid container justifyContent="flex-end">
             <Grid item>
               <Link href="#" variant="body2">
-               Sign in
+                Sign in
               </Link>
             </Grid>
           </Grid>
         </form>
       </div>
-      <Box mt={5}>
-       
-      </Box>
+      <Box mt={5}></Box>
     </Container>
   );
 }
--- server.js
+++ server.js
@@ -1,30 +1,34 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const path = require('path');
-const mongoose = require('mongoose');
-const routes =  require('./src/routes');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require("path");
+const mongoose = require("mongoose");
+const routes = require("./src/routes");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost:27017/tel-med',{
-    useUnifiedTopology:true,
-    useNewUrlParser:true,
-    useFindAndModify:false
-},function (err){
-    if(err){
-        console.log(err)
-    }else{
-        console.log('MongoDB CONECTADO com sucesso!')
+mongoose.connect(
+  "mongodb://localhost:27017/teste",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  },
+  function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("MongoDB CONECTADO com sucesso!");
     }
-});
+  }
+);
 
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(routes);
 
-app.listen(port,function(){
-    console.log(`serve runig on port${port}`)
+app.listen(port, function () {
+  console.log(`serve runig on port${port}`);
 });
